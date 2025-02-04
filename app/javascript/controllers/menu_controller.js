62bc0ce63bd9ee0619f047c819cef24110617d8a
felipeabdo <felipe.abdo.ti@.com>
2025-02-04 16:29:55 -0300

    ajuste de animação de menu parcial - desaparecimento

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -2,41 +2,40 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="menu"
 export default class extends Controller {
-  static targets = ["menu", "menuText"]
+  static targets = ["menu", "menuText", "icon"]
 
   connect() {
     console.log("Menu controller connected")
     this.state = 0 // Estado inicial do menu
   }
 
- animateMenu() {
+  animateMenu() {
     if (this.state === 0) {
-      // Ocultar nomes (a máscara cuidará disso em 0.2s)
+      // Adiciona a classe 'shrunk' para iniciar a ocultação dos nomes
       this.menuTarget.classList.add('shrunk')
 
-      // Ocultar ícones após 0.1s
+      // Ocultar ícones 0.2 segundos após o clique
       setTimeout(() => {
         this.iconTargets.forEach((icon) => {
           icon.classList.add('hidden')
         })
-      }, 100) // 0.1s em milissegundos
+      }, 200) // 200 milissegundos = 0.2 segundos
 
-      // Iniciar redução do menu após 0.2s
-      setTimeout(() => {
-        this.menuTarget.classList.add('first-animation')
-      }, 200) // 0.2s em milissegundos
-
-      // Após o menu atingir 80px, mostrar ícones novamente com animação
-      setTimeout(() => {
-        this.iconTargets.forEach((icon) => {
-          icon.classList.remove('hidden')
-          icon.classList.add('slide-in')
-        })
-      }, 1000) // Ajuste conforme o tempo da animação do menu
+      // O menu continua sua animação normalmente
+      this.menuTarget.classList.add('first-animation')
 
       this.state = 1
-    } else if (this.state === 1) {
-      // Estado para possíveis futuras interações
+    } else {
+      // Reseta para o estado inicial
+      this.menuTarget.classList.remove('first-animation', 'shrunk')
+      this.menuTarget.style.width = '250px'
+      this.menuTarget.style.visibility = 'visible'
+
+      // Remover a classe 'hidden' dos ícones para restaurá-los
+      this.iconTargets.forEach((icon) => {
+        icon.classList.remove('hidden')
+      })
+
       this.state = 0
     }
   }
