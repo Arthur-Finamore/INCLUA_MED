8838aa5ddc33a98b3c792e938d2f7f2ffbff0307
felipeabdo <felipe.abdo.ti@.com>
2025-02-05 17:40:01 -0300

    ajuste de animação de menu parcial - até 0px ok

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -47,29 +47,23 @@ export default class extends Controller {
 
   animateMenu() {
     if (this.state === 0) {
-      console.log("Iniciando animação do menu")
+      // Primeiro clique na seta quando o menu está expandido
+      console.log("Iniciando animação do menu para estado reduzido")
 
       // Passo 1: Animação da máscara
       this.menuTarget.classList.add('shrunk')
 
-      // Passo 2: Ocultar ícones após 0.2s
+      // Passo 2: Ocultar ícones e seta após 0.2s
       setTimeout(() => {
         this.iconTargets.forEach((icon) => {
           icon.classList.add('hidden')
         })
-
-        
         this.arrowTarget.classList.add('hidden')
-
-        setTimeout(() => {this.arrowTarget.classList.add('center-arrow')}, 200)
-        
-        
-        console.log("Ícones ocultados")
+        console.log("Ícones e seta ocultados")
       }, 200)
 
       // Passo 3: Iniciar animação de redução do menu
       this.menuTarget.classList.add('first-animation')
-
 
       // Passo 4: Remover textos do layout após 0.4s
       setTimeout(() => {
@@ -79,7 +73,7 @@ export default class extends Controller {
         console.log("Textos ocultados")
       }, 400)
 
-      // Passo 5: Reaparecer ícones e centralizar seta após 1.2s
+      // Passo 5: Reaparecer ícones e setas após 0.7s
       setTimeout(() => {
         this.iconTargets.forEach((icon) => {
           icon.classList.remove('hidden')
@@ -88,13 +82,11 @@ export default class extends Controller {
         this.arrowTarget.classList.remove('hidden')
         this.arrowForwardTarget.classList.remove('hidden')
 
-
-
         // Centralizar a seta
         this.arrowTarget.classList.add('center-arrow')
         this.arrowTarget.classList.add('arrow-height')
         this.menuTarget.classList.add('icons-visible')
-        console.log("Ícones reaparecidos e centralizados, seta centralizada")
+        console.log("Ícones e setas reaparecidos e centralizados")
       }, 700)
 
       // Passo 6: Remover textos do layout após 0.7s
@@ -106,11 +98,30 @@ export default class extends Controller {
       }, 700)
 
       this.state = 1
-    } else {
-      // Resetar para o estado inicial
-      console.log("Resetando menu")
 
-      this.menuTarget.classList.remove('first-animation', 'shrunk', 'icons-visible')
+    } else if (this.state === 1) {
+      // Segundo clique na seta quando o menu está reduzido
+      console.log("Iniciando animação do menu para ocultar completamente")
+
+      // Passo 1: Iniciar animação de redução total do menu
+      this.menuTarget.classList.add('hide-menu')
+
+      // Passo 2: Ocultar ícones e setas imediatamente
+      this.iconTargets.forEach((icon) => {
+        icon.classList.add('hidden')
+      })
+      this.arrowTarget.classList.add('hidden')
+      this.arrowForwardTarget.classList.add('hidden')
+      console.log("Ícones e setas ocultados")
+
+      this.state = 2
+
+    } else if (this.state === 2) {
+      // Clique para redefinir o menu para o estado inicial
+      console.log("Resetando menu para estado expandido")
+
+      // Remover todas as classes de animação e estilos inline
+      this.menuTarget.classList.remove('first-animation', 'shrunk', 'icons-visible', 'hide-menu')
       this.menuTarget.style.width = '250px'
       this.menuTarget.style.visibility = 'visible'
 
@@ -123,9 +134,8 @@ export default class extends Controller {
       })
 
       // Remover a centralização da seta
-      this.arrowTarget.classList.remove('center-arrow')
-      this.arrowTarget.classList.remove('arrow-height')
-      // Esconder a arrow-forward novamente
+      this.arrowTarget.classList.remove('center-arrow', 'arrow-height')
+      this.arrowTarget.classList.remove('hidden')
       this.arrowForwardTarget.classList.add('hidden')
 
       this.state = 0
